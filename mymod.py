ed7fe21e4c908b987abc49f51efd0b901564c2c4
Reuven Lerner <reuven@lerner.co.il>
2024-06-27 20:54:27 +0300

    gitautosync update 59

--- mymod.py
+++ mymod.py
@@ -1,4 +1,4 @@
-print('Hello from {__name__}!')
+print(f'Hello from {__name__}!')
 
 x = 100
 
@@ -7,4 +7,4 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}!'
 
-print('Goodbye from {__name__}!')
+print(f'Goodbye from {__name__}!')
